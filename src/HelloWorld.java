b140c2873c57a969455314d8e2879761a99ca60a
Kelvin <kelvin.daquon.wade@gmail.com>
2022-06-09 15:15:26 -0500

    update

---
+++ src/HelloWorld.java
@@ -0,0 +1,54 @@
+public class HelloWorld {
+    public static void  main (String[] args){
+        System.out.printf("Hello!, %s!", "World");
+
+//
+//
+//        byte tinyNum=127;
+//        System.out.println(tinyNum+1);
+//       final int num=235000;
+//       String empty="";
+//        long id= 1;
+//        System.out.println(num);
+//        System.out.println(id);
+//
+//        float floater = 8.27F;
+//        float noDecimal=9;
+//        System.out.println(noDecimal);
+//        System.out.println(floater);
+//
+//        String message="Hello world";
+
+        int myFavoriteNumner= 49;
+        System.out.println(myFavoriteNumner);
+        String myString="Hello,david!";
+        System.out.println(myString);
+        float myNumber= 3.14F;
+        System.out.println(myNumber);
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int xy = 5;
+        System.out.println(++xy);
+        System.out.println(xy);
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+
+        int z = 4;
+        z +=5;
+
+        int a = 3;
+        int b = 4;
+        b *=  a;
+
+        int c = 10;
+        int d = 2;
+        c /= d;
+        d -= c;
+
+
+    }
+}
