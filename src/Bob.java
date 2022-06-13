14fa2d585bf5427789a4a9e727fb7f342b55a1b8
Kelvin <kelvin.daquon.wade@gmail.com>
2022-06-13 11:31:06 -0500

    done

---
+++ src/Bob.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Hello I'm bob. Please talk to me!");
+        String input = scanner.nextLine();
+
+        if(input.endsWith("?")){
+            System.out.println("sure");
+        }
+        else if(input.endsWith("!")){
+            System.out.println("Whoa, chill out bro!");
+        }
+        else if (input.equals("")) {
+            System.out.println("Fine. Be that way!");
+        }
+        else{
+            System.out.println("Whatever.");
+        }
+    }
+}
