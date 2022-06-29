907477f6efac19019360a4c4989923d906d08ab5
Kelvin <kelvin.daquon.wade@gmail.com>
2022-06-29 15:49:23 -0500

    update

---
+++ src/until/InputTest.java
@@ -0,0 +1,20 @@
+package until;
+
+import static until.Input.getHex;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Input in = new Input();
+//        System.out.println(in.getDouble());
+//        in.getDouble(0.9, 9.9);
+//        in.getString();
+//        System.out.println(in.getInt());
+//        in.getInt(1,10 );
+//        System.out.println(in.yesNo());
+//        in.getString();
+//        System.out.println(getBinary("11111"));
+        System.out.println(getHex("10"));
+
+
+    }
+}
