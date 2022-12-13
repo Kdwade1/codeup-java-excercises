ed7a8344a742e259983ec4e1219f1cfb30d96532
Kelvin <kelvin.daquon.wade@gmail.com>
2022-12-12 18:23:57 -0600

    update

---
+++ src/practice/Scannner.java
@@ -0,0 +1,13 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Scannner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a= scan.nextInt();
+        System.out.println("value ="+a);
+
+    }
+
+}
