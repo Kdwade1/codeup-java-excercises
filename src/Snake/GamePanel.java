10c0a5bc300107626a4e978a1ffe2540ccd04323
Kelvin <kelvin.daquon.wade@gmail.com>
2022-12-14 16:37:57 -0600

    learning how to make snake

---
+++ src/Snake/GamePanel.java
@@ -0,0 +1,23 @@
+package Snake;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GamePanel extends JPanel implements ActionListener {
+    public GamePanel() {
+
+    }
+    public void startGame(){
+
+    }
+    public void paintComponent(Graphics g){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
