b5273150f29db282367a790eb33da4df8bbb8692
Kelvin <kelvin.daquon.wade@gmail.com>
2022-12-13 22:07:52 -0600

    finish gui lesson 1

--- src/practice/GUI.java
+++ src/practice/GUI.java
@@ -2,18 +2,47 @@ package practice;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
-    public GUI(){
-        JFrame jFrame = new JFrame();
+public class GUI implements ActionListener {
+    private int count = 0;
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
 
-        JPanel panel=new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
-        panel.setLayout(new GridLayout());
+    public GUI() {
+        frame = new JFrame();
+
+        JButton button = new JButton("Click Me");
+        button.addActionListener(this);
+
+         label = new JLabel("Number of clicks: 0");
+
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Our Gui");
+        frame.pack();
+        frame.setVisible(true);
+
     }
 
     public static void main(String[] args) {
         new GUI();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks: " + count);
+
 
     }
 }
